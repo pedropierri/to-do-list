7b1dfc48d80bca287114b1834bd32e506642f54a
Pedro Pierri <pedropierri.dev@gmail.com>
2025-09-10 18:26:00 -0300

    realizada logica basica add array checked tasks

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,13 @@ const inputNewTask = document.querySelector("#new-task")
 const addButton = document.querySelector("#btn-add")
 const taskList = document.querySelector("#tasks-list")
 const notification = document.querySelector("#notification")    
+const checkedTaskList = document.querySelector("#checked-tasks-list")
+const checkTasksButton = document.querySelector('#checked-tasks-btn')
 const taskArray = []
+const checkTaskArray = []
 
 addButton.addEventListener('click', addTask)
+checkTasksButton.addEventListener('click', checkTask)
 
 function addTask(event) {
     event.preventDefault()
@@ -28,22 +32,25 @@ function renderTasks() {
     taskList.innerHTML = ''
 
     for (let i = 0; i < taskArray.length; i++) {
-        console.log(i)
         const task = taskArray[i]
 
         const li = document.createElement("li")
         li.textContent = task
+        
+        const checkButton = document.createElement("button")
+        checkButton.textContent = "Check"
+        checkButton.setAttribute('data-index', i)
+        checkButton.addEventListener('click', checkTask)
+        checkButton.classList.add("check-btn")
+
 
         const deleteButton = document.createElement("button")
         deleteButton.textContent = "Remove Task"
         deleteButton.setAttribute('data-index', i)
         deleteButton.addEventListener('click', removeTask)
+        deleteButton.classList.add("delete-btn")
 
-        const checkButton = document.createElement("button")
-        checkButton.textContent = "Check"
-        checkButton.setAttribute('data-index', i)
-        checkButton.addEventListener('click', checkTask)
-        
+        li.appendChild(checkButton)
         li.appendChild(deleteButton)
 
         taskList.appendChild(li)
@@ -62,14 +69,15 @@ function removeTask(event) {
 function checkTask(event) {
     const index = event.target.getAttribute('data-index')
 
-    taskArray.splice(index, '')
+    checkTaskArray.push(taskArray[index])
+    taskArray.splice(index, 1)
 
     displayNotification("Task checked successfully", "success-msg")
     renderTasks()
 }
 
-function tasksRealizadas() {
-    
+function renderCheckedTasks() {
+       
 }
 
 function displayNotification(message, className) {
