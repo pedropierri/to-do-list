bc807eae3de40998d1ed3f1a6347064503daf527
Pedro Pierri <pedropmp05@gmail.com>
2025-09-10 15:45:24 -0300

    addTask, removeTask and renderTask functions finalized

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,9 @@
 // Selecionando elementos do html e declarando variáveis
 const inputNewTask = document.querySelector("#new-task")
 const addButton = document.querySelector("#btn-add")
-const taskList = document.querySelector("#task-list")
-const notification = document.querySelector("#notification")
+const taskList = document.querySelector("#tasks-list")
+const notification = document.querySelector("#notification")    
+const taskArray = []
 
 addButton.addEventListener('click', addTask)
 
@@ -10,11 +11,45 @@ function addTask(event) {
     event.preventDefault()
     
     if (inputNewTask.value !== '') {
-        let newTask = inputNewTask.value
-        notification.classList.toggle("sucess-msg")
-        notification.textContent = "Task added successfully"
+        let newTask = inputNewTask.value.trim()
+
+        taskArray.push(newTask) // Acrescenta task no array
+        inputNewTask.value = '' // Limpa o input
+
+        
+        renderTasks()
+        displayNotification("Tarefa adicionada com sucesso", "sucess-msg");
     } else {
         notification.classList.add("error-msg")
         notification.textContent = "Enter a task!"
     }   
+}
+
+function renderTasks() {
+    taskList.innerHTML = ''
+
+    for (let i = 0; i < taskArray.length; i++) {
+        console.log(i)
+        const task = taskArray[i]
+
+        const li = document.createElement("li")
+        li.textContent = task
+
+        const deleteButton = document.createElement("button")
+        deleteButton.textContent = "Remove Task"
+        deleteButton.setAttribute('data-index', i)
+        deleteButton.addEventListener('click', removeTask)
+        
+        li.appendChild(deleteButton)
+
+        taskList.appendChild(li)
+    }
+}
+
+function removeTask(event) {
+    const index = event.target.getAttribute('data-index')
+
+    taskArray.splice(index, 1)
+
+    renderTasks()
 }
